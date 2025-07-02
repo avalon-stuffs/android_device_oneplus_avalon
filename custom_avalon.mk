ca84b623b396dde94cda3a16b01fbe95083ea251
pure-soul-kk <krishnakripa34567@gmail.com>
2025-07-02 03:36:54 +0000

    avalon: Adapt to PixelOS
    Change-Id: I569a0ac1ab5b6776b593e973553ebb4a1eca3a2e
    Signed-off-by: K R I S H N A <puresoulkk.github@proton.me>

--- custom_avalon.mk
+++ custom_avalon.mk
@@ -10,10 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from avalon device
 $(call inherit-product, device/oneplus/avalon/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_avalon
+PRODUCT_NAME := custom_avalon
 PRODUCT_DEVICE := avalon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
